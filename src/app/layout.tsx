1c94637967eae91e00fd26a616b78d5f1ce4a88a
Michael <michaeldeanlindsay@gmail.com>
2025-01-03 15:30:33 -0800

    favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,9 @@ const geistMono = Geist_Mono({
 export const metadata: Metadata = {
   title: "NSM Marketing",
   description: "UTM generation tool.",
+  icons: {
+    icon: '/favicon.ico'
+  },
 };
 
 export default function RootLayout({
