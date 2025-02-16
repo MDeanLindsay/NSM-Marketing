4047511661f22943fdc3bf18656577cd4bc14e28
Michael <michaeldeanlindsay@gmail.com>
2025-02-15 22:54:09 -0800

    add landing page image

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,7 +7,14 @@ export default function Home() {
     <div className="min-h-screen flex flex-col">
       <Header />
       <div className="flex-1 overflow-auto pt-[75px]">
-        <main className="container mx-auto p-4 min-h-[calc(100vh-75px)]">
+        <main className="container mx-auto p-4 min-h-[calc(100vh-75px)] flex flex-col items-center justify-center">
+          <div className="rounded-full p-4 w-[550px] flex items-center justify-center">
+            <img 
+              src="/images/employee.png" 
+              alt="Employee illustration" 
+              className="w-full h-full object-contain"
+            />
+          </div>
           <Analytics/>
         </main>
       </div>
